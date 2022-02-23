04bff86229b37e951a32b37465679d4c3c03b9f4
sueta228 <maus1818@bk.ru>
2022-02-23 13:15:47 +0600

    commit

--- cyrcles.py
+++ cyrcles.py
@@ -1,18 +1,45 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5 import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 import sys
 from PyQt5 import uic
 from random import randint
-from PIL import Image
 
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(401, 449)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.create_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.create_btn.setGeometry(QtCore.QRect(90, 20, 221, 31))
+        self.create_btn.setObjectName("create_btn")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 401, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.create_btn.setText(_translate("MainWindow", "Создать флаг"))
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.do_paint = False
-        uic.loadUi('untitled.ui', self)  # Загружаем дизайн
-        self.create_btn.clicked.connect(self.paint)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.create_btn.clicked.connect(self.paint)
 
     def paintEvent(self, event):
         if self.do_paint:
